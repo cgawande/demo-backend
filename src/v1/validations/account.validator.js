f76d9b62021eee272d26bccd3a289b4848d07880
cgawande <vikug10@gmail.com>
2023-12-09 01:15:54 +0530

    permission features added

--- src/v1/validations/account.validator.js
+++ src/v1/validations/account.validator.js
@@ -1,62 +1,78 @@
-const Joi= require('joi');
+const Joi = require("joi");
 
-const emailOrMobile = Joi.alternatives().try(
-  Joi.string().trim().label('Phone Number').min(6)
-    .max(15)
-    .regex(/^\d+$/)
-    .empty()
-    .messages({
-      'string.pattern.base': 'ONLY_NUMERIC_ALLOWED',
-    }),
-  Joi.string().trim().label('Email')
-    .required(),
-).messages({
-  'alternatives.match': 'EMAIL_MOBILE_NUMBER_ALLOWED',
-});
+const emailOrMobile = Joi.alternatives()
+  .try(
+    Joi.string()
+      .trim()
+      .label("Phone Number")
+      .min(6)
+      .max(15)
+      .regex(/^\d+$/)
+      .empty()
+      .messages({
+        "string.pattern.base": "ONLY_NUMERIC_ALLOWED",
+      }),
+    Joi.string().trim().label("Email").required()
+  )
+  .messages({
+    "alternatives.match": "EMAIL_MOBILE_NUMBER_ALLOWED",
+  });
 
 const updateData = {
-  firstName: Joi.string().trim().min(3).max(24)
-    .label('First Name')
-    .required(),
-  lastName: Joi.string().trim().min(3).max(24)
-    .label('Last Name')
+  firstName: Joi.string().trim().min(3).max(24).label("First Name").required(),
+  lastName: Joi.string()
+    .trim()
+    .min(3)
+    .max(24)
+    .label("Last Name")
     .optional()
     .empty()
     .allow(null)
-    .allow(''),
-  profilePicture: Joi.string().trim().optional().empty()
-    .allow(''),
-  phoneNumber: Joi.string().label('Phone Number').min(6)
+    .allow(""),
+  profilePicture: Joi.string().trim().optional().empty().allow(""),
+  phoneNumber: Joi.string()
+    .label("Phone Number")
+    .min(6)
     .max(12)
     .regex(/^\d+$/)
     .empty()
     .messages({
-      'string.pattern.base': 'ONLY_NUMERIC_ALLOWED',
+      "string.pattern.base": "ONLY_NUMERIC_ALLOWED",
     }),
-  email: Joi.string().trim().email({ minDomainSegments: 2, tlds: { allow: false } }).min(6)
+  email: Joi.string()
+    .trim()
+    .email({ minDomainSegments: 2, tlds: { allow: false } })
+    .min(6)
     .max(100)
     .required(),
 };
 
 const loginDetail = {
-  email: Joi.string().trim().email({ minDomainSegments: 2, tlds: { allow: false } }).label('Email')
+  email: Joi.string()
+    .trim()
+    .email({ minDomainSegments: 2, tlds: { allow: false } })
+    .label("Email")
     .required(),
-  password: Joi.string().trim().label('Password')
-    .required(),
+  password: Joi.string().trim().label("Password").required(),
 };
 
 const passwordCommon = {
-  newPassword: Joi.string().trim()
+  newPassword: Joi.string()
+    .trim()
     .min(6)
     .max(15)
-    .regex(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/)
+    .regex(
+      /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/
+    )
     .messages({
-      'string.pattern.base': 'PASSWORD_VALIDATION',
+      "string.pattern.base": "PASSWORD_VALIDATION",
     })
     .required(),
-  confirmPassword: Joi.string().trim().label('Confirm Password')
-    .valid(Joi.ref('newPassword'))
-    .messages({ 'any.only': 'CONFIRM_PASSWORD_NOT_MATCH' })
+  confirmPassword: Joi.string()
+    .trim()
+    .label("Confirm Password")
+    .valid(Joi.ref("newPassword"))
+    .messages({ "any.only": "CONFIRM_PASSWORD_NOT_MATCH" })
     .required(),
 };
 
@@ -64,9 +80,11 @@ const userAccountSignupSchema = {
   body: Joi.object().keys({
     ...updateData,
     ...loginDetail,
-    confirmPassword: Joi.string().trim().label('Confirm Password')
-      .valid(Joi.ref('password'))
-      .messages({ 'any.only': 'CONFIRM_PASSWORD_NOT_MATCH' })
+    confirmPassword: Joi.string()
+      .trim()
+      .label("Confirm Password")
+      .valid(Joi.ref("password"))
+      .messages({ "any.only": "CONFIRM_PASSWORD_NOT_MATCH" })
       .required(),
   }),
 };
@@ -78,36 +96,23 @@ const editProfileSchema = {
 const editProfileDetailsSchema = {
   body: Joi.object().keys({
     ...updateData,
-    city: Joi.string().trim().min(2).max(30)
-      .optional()
-      .empty()
-      .allow(''),
-    state: Joi.string().trim().min(2).max(30)
-      .optional()
-      .empty()
-      .allow(''),
-    country: Joi.string().trim().min(2).max(20)
-      .optional()
-      .empty()
-      .allow(''),
-    address: Joi.string().trim().min(2).max(60)
-      .optional()
-      .empty()
-      .allow(''),
-    dateOfBirth: Joi.string().trim().optional().empty()
-      .allow(''),
+    city: Joi.string().trim().min(2).max(30).optional().empty().allow(""),
+    state: Joi.string().trim().min(2).max(30).optional().empty().allow(""),
+    country: Joi.string().trim().min(2).max(20).optional().empty().allow(""),
+    address: Joi.string().trim().min(2).max(60).optional().empty().allow(""),
+    dateOfBirth: Joi.string().trim().optional().empty().allow(""),
     gender: Joi.string()
-      .valid('male', 'female', 'other')
+      .valid("male", "female", "other")
       .optional()
       .empty()
-      .allow(''),
+      .allow(""),
   }),
 };
 
 const userAdminAccountLoginSchema = {
   body: Joi.object().keys({
     ...loginDetail,
-    userRole: Joi.string().trim().valid('admin', 'staff').required(),
+    userRole: Joi.string().trim().valid("admin", "staff").required(),
   }),
 };
 
@@ -126,13 +131,16 @@ const userAccountResetPasswordSchema = {
 const userAccountChangePasswordSchema = {
   body: Joi.object().keys({
     password: Joi.string().trim().required(),
-    newPassword: Joi.string().trim()
-    .min(6)
-    .max(15)
-    .regex(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/)
-    .messages({
-      'string.pattern.base': 'PASSWORD_VALIDATION',
-    })
+    newPassword: Joi.string()
+      .trim()
+      .min(6)
+      .max(15)
+      .regex(
+        /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/
+      )
+      .messages({
+        "string.pattern.base": "PASSWORD_VALIDATION",
+      }),
   }),
 };
 
@@ -147,72 +155,87 @@ const sellerAccountLoginSchema = {
 const userAccountLoginSchema = {
   body: Joi.object({
     ...loginDetail,
-    email: Joi.alternatives().try(
-      Joi.string().trim().label('Phone Number').min(10)
-        .max(12)
-        .regex(/^\d+$/)
-        .empty()
-        .messages({
-          'string.pattern.base': 'ONLY_NUMERIC_ALLOWED',
-        }),
-      Joi.string().trim().email({ minDomainSegments: 2, tlds: { allow: false } }).label('Email')
-        .required(),
-    ).messages({
-      'alternatives.match': 'EMAIL_MOBILE_NUMBER_ALLOWED',
-    }),
-    firebaseToken: Joi.string().trim().optional().empty()
-      .allow(''),
-    deviceType: Joi.string().trim().valid('android', 'ios').required(),
+    email: Joi.alternatives()
+      .try(
+        Joi.string()
+          .trim()
+          .label("Phone Number")
+          .min(10)
+          .max(12)
+          .regex(/^\d+$/)
+          .empty()
+          .messages({
+            "string.pattern.base": "ONLY_NUMERIC_ALLOWED",
+          }),
+        Joi.string()
+          .trim()
+          .email({ minDomainSegments: 2, tlds: { allow: false } })
+          .label("Email")
+          .required()
+      )
+      .messages({
+        "alternatives.match": "EMAIL_MOBILE_NUMBER_ALLOWED",
+      }),
+    firebaseToken: Joi.string().trim().optional().empty().allow(""),
+    deviceType: Joi.string().trim().valid("android", "ios").required(),
     appVersion: Joi.string().trim().required(),
   }),
 };
 
-
-
 const userAccountSignUpSchema = {
   body: Joi.object({
-  //   firstName: Joi.string().trim().min(3).max(24)
-  //   .label('First Name')
-  //   .required(),
-  // lastName: Joi.string().trim().min(3).max(24)
-  //   .label('Last Name')
-  //   .optional()
-  //   .empty()
-  //   .allow(null)
-  //   .allow(''),
-     fullName: Joi.string().trim().min(4).max(100)
-    .label('full Name')
-    .required(),
-  phoneNumber:  Joi.number().integer().min(10 ** 9).max(10 ** 10 - 1).required().messages({
-    'number.min': 'Mobile number should be 10 digit.',
-    'number.max': 'Mobile number should be 10 digit'
-}),
-  profilePicture: Joi.string().trim().optional().empty()
-    .allow(''),
-  password: Joi.string().trim()
-  .min(6)
-  .max(15)
-  .regex(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/)
-  .messages({
-    'string.pattern.base': 'PASSWORD_VALIDATION',
-  })
-  .required(),
-  confirmPassword: Joi.string().trim().label('Confirm Password')
-  .valid(Joi.ref('password'))
-  .messages({ 'any.only': 'CONFIRM_PASSWORD_NOT_MATCH' })
-  .required(),
-  email: Joi.string().trim().email({ minDomainSegments: 2, tlds: { allow: false } }).min(6)
-    .max(100)
-    .required(),
+    //   firstName: Joi.string().trim().min(3).max(24)
+    //   .label('First Name')
+    //   .required(),
+    // lastName: Joi.string().trim().min(3).max(24)
+    //   .label('Last Name')
+    //   .optional()
+    //   .empty()
+    //   .allow(null)
+    //   .allow(''),
+    fullName: Joi.string().trim().min(4).max(100).label("full Name").required(),
+    phoneNumber: Joi.number()
+      .integer()
+      .min(10 ** 9)
+      .max(10 ** 10 - 1)
+      .required()
+      .messages({
+        "number.min": "Mobile number should be 10 digit.",
+        "number.max": "Mobile number should be 10 digit",
+      }),
+    profilePicture: Joi.string().trim().optional().empty().allow(""),
+    password: Joi.string()
+      .trim()
+      .min(6)
+      .max(15)
+      .regex(
+        /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{6,}).*$/
+      )
+      .messages({
+        "string.pattern.base": "PASSWORD_VALIDATION",
+      })
+      .required(),
+    confirmPassword: Joi.string()
+      .trim()
+      .label("Confirm Password")
+      .valid(Joi.ref("password"))
+      .messages({ "any.only": "CONFIRM_PASSWORD_NOT_MATCH" })
+      .required(),
+    email: Joi.string()
+      .trim()
+      .email({ minDomainSegments: 2, tlds: { allow: false } })
+      .min(6)
+      .max(100)
+      .required(),
+    permissions: Joi.array().optional(),
   }),
-  permissions:Joi.array().optional()
 };
 const userUpdateRoleSchema = {
   body: Joi.object().keys({
-    role: Joi.string().trim().valid("user","sub-admin").required(),
+    role: Joi.string().trim().valid("user", "sub-admin").required(),
   }),
 };
-module.exports ={
+module.exports = {
   userAccountSignupSchema,
   userAdminAccountLoginSchema,
   userAccountForgetPasswordSchema,
@@ -223,5 +246,5 @@ module.exports ={
   sellerAccountLoginSchema,
   editProfileDetailsSchema,
   userAccountSignUpSchema,
-  userUpdateRoleSchema
+  userUpdateRoleSchema,
 };
